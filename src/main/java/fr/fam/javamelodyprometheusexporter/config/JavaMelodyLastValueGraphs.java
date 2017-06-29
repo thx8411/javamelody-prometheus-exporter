691f2f99f9fd75df1c9c87925b2177f9e50e6910
Blaise-Florentin Collin <thx8411@yahoo.fr>
2017-06-29 15:07:10 +0200

    checkstyle fix

--- src/main/java/fr/fam/javamelodyprometheusexporter/config/JavaMelodyLastValueGraphs.java
+++ src/main/java/fr/fam/javamelodyprometheusexporter/config/JavaMelodyLastValueGraphs.java
@@ -5,28 +5,51 @@ package fr.fam.javamelodyprometheusexporter.config;
 */
 public enum JavaMelodyLastValueGraphs {
 
+    /** */
     HTTP_HITS_RATE("httpHitsRate"),
+    /** */
     HTTP_MEAN_TIMES("httpMeanTimes"),
+    /** */
     HTTP_SYSTEM_ERRORS("httpSystemErrors"),
+    /** */
     TOMCAT_BUSY_THREADS("tomcatBusyThreads"),
+    /** */
     TOMCAT_BYTES_RECEIVED("tomcatBytesReceived"),
+    /** */
     TOMCAT_BYTES_SENT("tomcatBytesSent"),
+    /** */
     USED_MEMORY("usedMemory"),
+    /** */
     CPU("cpu"),
+    /** */
     HTTP_SESSIONS("httpSessions"),
+    /** */
     ACTIVE_THREADS("activeThreads"),
+    /** */
     ACTIVE_CONNECTIONS("activeConnections"),
+    /** */
     USED_CONNECTIONS("usedConnections"),
+    /** */
     GARBAGE_COLLECTOR("gc"),
+    /** */
     THREAD_COUNT("threadCount"),
+    /** */
     LOADED_CLASSES_COUNT("loadedClassesCount"),
+    /** */
     USED_NON_HEAP_MEMORY("usedNonHeapMemory"),
+    /** */
     USED_PHYSICAL_MEMORY_SIZE("usedPhysicalMemorySize"),
+    /** */
     USED_SWAP_SPACE_SIZE("usedSwapSpaceSize"),
+    /** */
     HTTP_SESSIONS_MEAN_AGE("httpSessionsMeanAge"),
+    /** */
     SQL_HITS_RATE("sqlHitsRate"),
+    /** */
     SQL_MEAN_TIMES("sqlMeanTimes"),
+    /** */
     SQL_SYSTEM_ERRORS("sqlSystemErrors"),
+    /** */
     OPEN_FILES("fileDescriptors");
 
     /**
